3d88690c9068b0caea8542e0d3d69ad4c35bc9ae
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-30 15:43:02 -0300

    Custom authentication backend implemented close #17

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -125,3 +125,9 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+
+]
