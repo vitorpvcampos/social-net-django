b6afcc5af04cc084783a00b57766b6b4165cfb3a
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-30 15:06:47 -0300

    Messages framework configured close #15

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Profile
@@ -52,6 +53,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfuly.')
+        else:
+            messages.error(request, 'Error updating your profile.')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
