99f3a420540c0362485ac7c2b7d61968c561fb34
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-09-30 16:42:42 -0300

    Social authentication preparation done close #19

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
