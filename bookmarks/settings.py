db4b4b22e345f3c2710dc401842ffd4edcb4bd5a
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-10-26 11:47:33 -0300

    Redis integrated close #78

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -151,3 +151,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
